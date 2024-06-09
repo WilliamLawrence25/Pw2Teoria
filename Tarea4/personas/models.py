5f3a1d27ff8be82c8face25451ffa8d7a7162889
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-06-08 23:11:07 -0500

    Arreglango problema con la actualizacion de registros anteriores: - Se agrego un default al nuevo dato

--- Tarea4/personas/models.py
+++ Tarea4/personas/models.py
@@ -6,4 +6,4 @@ class Persona(models.Model):
     nombres = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
     edad = models.IntegerField() #(max_digits = 3)
-    donador = models.CharField(max_length=100)
+    donador = models.BooleanField(default=False)
