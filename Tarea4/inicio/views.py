30358456046d2832dffef548378c2a64593940e9
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-06-11 07:43:25 -0500

    Nueva vista agragada para la URL another

--- Tarea4/inicio/views.py
+++ Tarea4/inicio/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def myHomeView(*args, **kwargs):
     return HttpResponse('<h1>Hola mundo desde Django</h1>')
+
+def anotherView(request):
+    return HttpResponse('<h1>Solo otra pagina</h1>')
