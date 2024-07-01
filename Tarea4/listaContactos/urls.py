4450d159e8983ace1c6118513992d5b2089952ec
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-07-01 17:26:45 -0500

    Agregando la url para persona

--- Tarea4/listaContactos/urls.py
+++ Tarea4/listaContactos/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import  anotherView
+from personas.views import personaTestView
 
 urlpatterns = [
-    path('', myHomeView, name='Pagina de inicio'),
-    path('another/', anotherView),
+    path('', myHomeView, name='home'),
+    path('another/', anotherView, name='otro'),
+    path('persona/', personaTestView, name='otro'),
     path('admin/', admin.site.urls),
 
 ]
