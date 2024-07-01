17af2082f54020f16d3b9cc05dfcd26d94b45e8d
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-07-01 14:11:21 -0500

    retornando el home.html de templates

--- Tarea4/inicio/views.py
+++ Tarea4/inicio/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 
 def myHomeView(request, *args, **kwargs):
     print (args, kwargs)
     print (request.user)
-    return HttpResponse('<h1>Hola mundo desde Django</h1>')
+    return render(request, "home.html", {})
 
 def anotherView(request):
     return HttpResponse('<h1>Solo otra pagina</h1>')
