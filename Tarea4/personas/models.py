27874e8bd37e30e63b2cbf3983fa05398ae3cc94
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-07-01 17:26:57 -0500

    error fixed

--- Tarea4/personas/models.py
+++ Tarea4/personas/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Persona(models.Model):
     nombres = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
-    edad = models.IntegerField() #(max_digits = 3)
+    edad = models.IntegerField(default=18) #(max_digits = 3)
     donador = models.BooleanField(default=True)
